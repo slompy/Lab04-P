9ea4e6a993774e74629611f975326a923ed14313
slompy <krakenov.slava@mail.ru>
2026-09-18 10:29:18 +0300

    Lab04: start

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// int score = int.Parse(Console.ReadLine()!);
+
+// if (score >= 1000) {
+//     System.Console.WriteLine("Бонусный уровень открыт");
+// }
+// else {
+//     System.Console.WriteLine("Бонусный уровень закрыт");
+// }
+
+// System.Console.WriteLine("Игра продолжается");
+
+// int score = int.Parse(Console.ReadLine()!);
+
+// if (score < 500) {
+//     Console.WriteLine("Новичок")
+// } else if (score < 2000) {
+//     System.Console.WriteLine("Боец");
+// } else {
+//     System.Console.WriteLine("Мастер");
+// }
+
+// int score = int.Parse(Console.ReadLine()!);
+// int lives = 2;
+// bool cheatCode = true;
+// if (score >= 1000 && lives >= 1) {
+//     System.Console.WriteLine("Секретный уровень открыт");
+// } else if (cheatCode == true) {
+//     System.Console.WriteLine("Секретный уровень открыт");
+// } else {
+//     System.Console.WriteLine("Пока нельзя");
+// }
+
+// int movieNumber = int.Parse(Console.ReadLine()!);
+
+// string movieGenre;
+
+// switch (movieNumber)
+// {
+//     case 1: movieGenre = "Боевик"; break;
+//     case 2: movieGenre = "Комедия"; break;
+//     case 3: movieGenre = "Драма"; break;
+    
+//     default: movieGenre = "Другой жанр"; break;
+// }
+
+// Console.WriteLine(movieGenre);
+
+int score = 3200;
+int recordScore = 3000;
+
+string result = (score > recordScore) ? "Новый рекорд" : "Обычный результат";
+
+
+// if(score > recordScore) {
+//     result = "Новый рекорд";
+// } else {
+//     result = "Обычный результат!";
+// }
+Console.WriteLine(result);
